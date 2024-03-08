db9d0cfe0cebf92bdf825a6d635d111ec2615487
Akhilesh Trivedi <atakhileshtrivedi@gmail.com>
2024-03-08 20:35:22 +0530

    added in triggering file

--- src/main/java/com/at/stripepayment/StripePaymentApplication.java
+++ src/main/java/com/at/stripepayment/StripePaymentApplication.java
@@ -1,8 +1,11 @@
 package com.at.stripepayment;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(info = @Info(title = "Stripe API", version = "1.0"))
 @SpringBootApplication
 public class StripePaymentApplication {
 
